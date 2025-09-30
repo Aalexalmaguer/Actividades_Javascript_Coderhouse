ad91f811029b09217c9f2df58ab9eaea80bf7cf6
aalexalmaguer <alejandroalmaguer2001@gmail.com>
2025-09-30 09:17:42 -0600

    Se añadieron ejercicios de Actividad Practica 8

---
+++ Actividad_Practica_8/Actividad_Practica_8.js
@@ -0,0 +1,10 @@
+document.getElementById("botoncin").addEventListener("click", function() {
+    Toastify({
+        text: "Me clickeaste perro, clickeame de nuevo si quieres ver una sorpresa",
+        duration: 10000,
+        gravity: "top",
+        position: "center",
+        backgroundColor: "blue",
+        destination: "https://youtu.be/xvFZjo5PgG0?si=clljwXG_0JkPDFxD"
+    }).showToast();
+})
